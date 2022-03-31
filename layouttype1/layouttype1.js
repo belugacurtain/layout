7a467d9cbeae04506d8eaab52822c1fae0730b11
gunami <58508367+gunami4573@users.noreply.github.com>
2022-03-31 16:44:18 +0900

    Gunami Layout Type1

--- layouttype1/layouttype1.js
+++ layouttype1/layouttype1.js
@@ -3,7 +3,16 @@
 
     $(function() {
 
-        //write
+        //nav nav_itme hover
+        var $NavList = $('.nav_list'),
+            $NavItem = $NavList.find('.nav_item');
+        $NavItem.on('mouseenter', function(){
+            $NavList.css('animation-play-state','paused');
+
+        });
+        $NavItem.on('mouseleave', function(){
+            $NavList.css('animation-play-state','running');
+        });
 
 
 
