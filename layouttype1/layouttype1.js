1cccb73db807ed81875caf6f842d8fe9f6f9229f
gunami <58508367+gunami4573@users.noreply.github.com>
2022-03-31 17:07:09 +0900

    Gunami Layout Type1

--- layouttype1/layouttype1.js
+++ layouttype1/layouttype1.js
@@ -1,19 +1,32 @@
 (function($) {
     'use strict';
 
+    var $Html = $('html');
+
     $(function() {
 
         //nav nav_itme hover
-        var $NavList = $('.nav_list'),
+        var $LogoBox = $('.logo_box'),
+            $NavList = $('.nav_list'),
             $NavItem = $NavList.find('.nav_item');
+        $LogoBox.on('mouseenter', function(){
+            $Html.addClass('menu_open');
+        });
+        $LogoBox.on('mouseleave', function(){
+            $Html.removeClass('menu_open');
+        });
         $NavItem.on('mouseenter', function(){
             $NavList.css('animation-play-state','paused');
-
         });
         $NavItem.on('mouseleave', function(){
             $NavList.css('animation-play-state','running');
         });
 
+
+        var $IsSectionTop = $NavItem.is('nav_item.top'),
+            $IsSectionRight = $NavItem.is('nav_item.right'),
+            $IsSectionBottom = $NavItem.is('nav_item.bottom'),
+            $IsSectionLeft = $NavItem.is('nav_item.left');
 
 
     });
