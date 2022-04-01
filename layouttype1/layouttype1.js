f12ba5e275dc78e4b364375aa1502cad515f78a2
gunami <58508367+gunami4573@users.noreply.github.com>
2022-04-01 16:26:25 +0900

    Gunami Layout Type1

--- layouttype1/layouttype1.js
+++ layouttype1/layouttype1.js
@@ -9,11 +9,13 @@
         var $LogoBox = $('.logo_box'),
             $NavList = $('.nav_list'),
             $NavItem = $NavList.find('.nav_item');
+
         $LogoBox.on('mouseenter', function(){
             $Html.addClass('menu_open');
         });
         $LogoBox.on('mouseleave', function(){
             $Html.removeClass('menu_open');
+            $NavItem.removeClass('active');
         });
         $NavItem.on('mouseenter', function(){
             $NavList.css('animation-play-state','paused');
@@ -23,11 +25,27 @@
         });
 
 
-        var $IsSectionTop = $NavItem.is('nav_item.top'),
-            $IsSectionRight = $NavItem.is('nav_item.right'),
-            $IsSectionBottom = $NavItem.is('nav_item.bottom'),
-            $IsSectionLeft = $NavItem.is('nav_item.left');
-
+        var IsSectionTop = $NavItem.is('.nav_item.top'),
+            IsSectionRight = $NavItem.is('.nav_item.right'),
+            IsSectionBottom = $NavItem.is('.nav_item.bottom'),
+            IsSectionLeft = $NavItem.is('.nav_item.left');
+        if(IsSectionTop){
+            var $this = $(this),
+                $NavBtn = $this.find('.top button');
+            $NavBtn.on('click', function(){
+               var $this = $(this),
+                   $MyNavItem = $this.parent('.nav_item'),
+                   IsActive = $MyNavItem.is('.active');
+               if(!IsActive){
+                   setTimeout(function(){
+                       $MyNavItem.addClass('active');
+                   }, 200)
+               }
+               else{
+                   $MyNavItem.removeClass('active');
+               }
+            });
+        }
 
     });
 })(jQuery);
