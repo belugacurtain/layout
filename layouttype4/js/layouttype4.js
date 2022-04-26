579d6f88f5af4c3083f8f2263aa8008099f19cd5
gunami <58508367+gunami4573@users.noreply.github.com>
2022-04-26 17:10:45 +0900

    Gunami Layout Type4 (No Slide Plugin)

--- layouttype4/js/layouttype4.js
+++ layouttype4/js/layouttype4.js
@@ -1,8 +1,9 @@
 window.onload = function(){
+
+    //
     var Wrapper = document.getElementById('wrapper'),
         LnbBtn = document.querySelector('.lnb_btn');
     LnbBtn.addEventListener("click", function () {
-        //if lnb_open classname have
         if (Wrapper.classList.contains('lnb_open')) {
             Wrapper.classList.remove('lnb_open');
             LnbBtn.setAttribute('title', 'menu open')
@@ -12,48 +13,79 @@ window.onload = function(){
         }
     });
 
+    //
     var Container = document.getElementById('container'),
         ContainerWidth = Container.scrollWidth,
         ContainerWidthPer = (ContainerWidth / ContainerWidth) * 100,
-        Section = document.getElementsByTagName('section');
+        SectionWrap = document.getElementsByClassName('section_wrap'),
+        Section = document.getElementsByClassName('section'),
+        SectionArea = ContainerWidthPer / Section.length;
+    console.log(Section.length);
+    console.log(SectionArea);
 
-    console.log(ContainerWidthPer);
-
+    //
     function ContainerMove(event){
         Container.style.transform = "translateX(-"+event+"%)";
     }
+    function SectionScale(event, index){
+        SectionWrap[index].style.transform = "scale("+event+")";
+    }
 
+    //
     var move, count = 0;
-
     window.onmousewheel = function (e) {
         if(e.wheelDelta === -120){
             count++;
-            move = (count/80)*100;
-
-            if(move >= ContainerWidthPer){
-                ContainerMove(100);
-                move = 100;
-                count = 80;
+            move = (count / ContainerWidthPer) * ContainerWidthPer;
+            console.log(move);
+            if(move > ContainerWidthPer){
+                ContainerMove(ContainerWidthPer);
+                move = ContainerWidthPer;
+                count = ContainerWidthPer;
             }
-            if(move < ContainerWidthPer){
+            if(move <= ContainerWidthPer){
                 ContainerMove(move);
+                var sectionnumber = count / SectionArea;
+                console.log(sectionnumber);
+                if(sectionnumber <= 1){
+                    SectionScale((1 - (count/SectionArea)), 0);
+                }
+                if(sectionnumber > 1 && sectionnumber <= 2 ){
+                    SectionScale((2 - sectionnumber), 1);
+                }
+                if(sectionnumber > 2 && sectionnumber <= 3 ){
+                    SectionScale((3 - sectionnumber), 2);
+                }
+                if(sectionnumber > 3 && sectionnumber <= 4 ){
+                    SectionScale((4 - sectionnumber), 3);
+                }
             }
         }
         else{
             count--;
-            move = (count/80)*100;
-
+            move = (count / ContainerWidthPer) * ContainerWidthPer;
             if(move < 0){
                 ContainerMove(0);
                 move = 0;
                 count = 0;
             }
-
-            ContainerMove(move);
+            if(move >= 0){
+                ContainerMove(move);
+                var sectionnumber = count / SectionArea;
+                console.log(sectionnumber);
+                if(sectionnumber < 1 && sectionnumber >= 0){
+                    SectionScale((1 - (count/SectionArea)), 0);
+                }
+                if(sectionnumber < 2 && sectionnumber >= 1){
+                    SectionScale((2 - (count/SectionArea)), 1);
+                }
+                if(sectionnumber < 3 && sectionnumber >= 2){
+                    SectionScale((3 - (count/SectionArea)), 2);
+                }
+                if(sectionnumber < 4 && sectionnumber >= 3){
+                    SectionScale((4 - (count/SectionArea)), 3);
+                }
+            }
         }
     }
-
-    var SectionBtn = document.getElementsByClassName('btn');
-
-
 }   //window.onload end
