f9498c5690b26bc3cc3ee32c768cd0c7152b43e6
gunami <58508367+gunami4573@users.noreply.github.com>
2022-04-26 09:02:19 +0900

    Gunami Layout Type4 (No Slide Plugin)

--- layouttype4/js/layouttype4.js
+++ layouttype4/js/layouttype4.js
@@ -1,54 +1,4 @@
-/*
-(function($){
-    'use strict';
-
-    $(function(){
-
-        var $window = $(window);
-
-        $window.on('scroll', function(){
-            var ScrollTop = $window.scrollTop();
-            var $Body = $('body'),
-                $Header = $('#header'),
-                $Container = $('#container'),
-                BodyOffset = $Body.offset(),
-                BodyTop = BodyOffset.top,
-                ContainerOffset = $Container.offset(),
-                ContainerTop = ContainerOffset.top,
-                ContainerHeight = ContainerTop + $Container.innerHeight();
-
-
-            if(ScrollTop <= 300){
-                console.log(ScrollTop);
-                $Header.css({'height':''+ScrollTop+'px'});
-            }
-
-            if($Header.innerHeight() == 300 ){
-                console.log('300');
-            }
-        });
-
-    });
-})(jQuery);
-*/
-
-$(document).ready(function () {
-    /*
-    const circle = document.querySelector('.circle');
-    document.addEventListener("mousemove", (e) => {
-        const mouseX = e.clientX;
-        const mouseY = e.clientY;
-        circle.style.width = '64px';
-        circle.style.height = '64px';
-        circle.style.top = mouseY + 'px';
-        circle.style.left = mouseX + 'px';
-    });
-    document.addEventListener("mouseout", (e) => {
-        circle.style.width = '0px';
-        circle.style.height = '0px';
-    });
-    */
-
+window.onload = function(){
     var Wrapper = document.getElementById('wrapper'),
         LnbBtn = document.querySelector('.lnb_btn');
     LnbBtn.addEventListener("click", function () {
@@ -95,11 +45,15 @@ $(document).ready(function () {
 
             if(move < 0){
                 ContainerMove(0);
-                move=0;
-                count=0;
+                move = 0;
+                count = 0;
             }
 
             ContainerMove(move);
         }
     }
-});
+
+    var SectionBtn = document.getElementsByClassName('btn');
+
+
+}   //window.onload end
